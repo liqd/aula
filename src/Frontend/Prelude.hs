e03d4f67897b9603995bde7d322e66cf18f05ad8
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-02-16 17:18:32 +0100

    Frontend.Prelude.for_: hiding the one from Lucid keeping the one from Foldable

--- src/Frontend/Prelude.hs
+++ src/Frontend/Prelude.hs
@@ -1,14 +1,14 @@
 module Frontend.Prelude (module X) where
 
-import Control.Monad.Trans.Except as X (ExceptT) 
-import Lucid                      as X 
-import Servant                    as X 
-import Servant.HTML.Lucid         as X 
+import Control.Monad.Trans.Except as X (ExceptT)
+import Lucid                      as X hiding (for_)
+import Servant                    as X
+import Servant.HTML.Lucid         as X
 import Servant.Missing            as X
 import Persistent                 as X
 import Text.Digestive.Form        as X ((.:))
 import Text.Digestive.View        as X (View)
 import Test.QuickCheck            as X (generate, arbitrary)
-import Thentos.Prelude            as X hiding (for_)
+import Thentos.Prelude            as X
 import Types                      as X
 import Frontend.Core              as X
