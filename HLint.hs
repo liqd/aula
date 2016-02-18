444fa2a4156ab73437b66c0ca3d43f14c0fb898d
Matthias Fischmann <mf@zerobuzz.net>
2016-02-18 18:02:49 +0100

    Provoke some hlint errors (to test that travis will notice).

--- HLint.hs
+++ HLint.hs
@@ -2,10 +2,3 @@ import "hlint" HLint.Default
 import "hlint" HLint.Dollar
 import "hlint" HLint.Generalise
 import "hlint" HLint.HLint
-
-ignore "Redundant do"
-ignore "Use const"
-ignore "Use fmap"
-ignore "Use list literal"
-ignore "Use mappend"
-ignore "Use record patterns"
