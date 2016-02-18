9e33054f9c755adcfe3dbc5ef808c109b2ad2d1a
Matthias Fischmann <mf@zerobuzz.net>
2016-02-18 18:04:08 +0100

    Revert "Provoke some hlint errors (to test that travis will notice)."
    This reverts commit 444fa2a4156ab73437b66c0ca3d43f14c0fb898d.

--- HLint.hs
+++ HLint.hs
@@ -2,3 +2,10 @@ import "hlint" HLint.Default
 import "hlint" HLint.Dollar
 import "hlint" HLint.Generalise
 import "hlint" HLint.HLint
+
+ignore "Redundant do"
+ignore "Use const"
+ignore "Use fmap"
+ignore "Use list literal"
+ignore "Use mappend"
+ignore "Use record patterns"
