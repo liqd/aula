eff760509545c420b9dc873fa6c7ff0e2a77f4bb
Matthias Fischmann <mf@zerobuzz.net>
2016-02-21 11:17:04 +0100

    Shuffle code around to tell a more coherent story (otherwise no changes).

--- src/Action.hs
+++ src/Action.hs
@@ -9,17 +9,19 @@
 
 -- | The 'Action' module contains an API which
 module Action
-    ( ActionError
+    ( -- * constraint types
+      ActionM
+    , ActionLog(logEvent)
+    , ActionPersist(persistent)
+    , ActionUserHandler(login, logout)
+    , ActionError
+
+      -- * concrete monad type (abstract)
     , Action
-    , ActionUserHandler(..)
-    , ActionLog(..)
-    , ActionPersist(..)
-    , ActionM
     , mkRunAction
 
-    , UserState(UserLoggedOut, UserLoggedIn)
-    , sessionCookie
-    , username
+      -- * user state
+    , UserState(UserLoggedOut, UserLoggedIn), sessionCookie, username
     )
 where
 
@@ -33,19 +35,58 @@ import Persistent
 import Prelude hiding (log)
 import Servant
 
+
 ----------------------------------------------------------------------
--- User behaviour
+-- constraint types
 
--- | Top level errors can happen.
---
--- FIXME: Create a different type
-type ActionExcept = ServantErr
+class ( ActionLog m
+      , ActionPersist m
+      , ActionUserHandler m
+      , ActionError m
+      , MonadIO m  -- FIXME: can we get rid of this?  (it is needed in "CreateRandom", but also for
+                   -- 'ActionLog', 'ActionPersist'.)
+      ) => ActionM m
 
--- | User representation during an action
--- FIXME: Figure out which information is needed here.
-data UserState
-    = UserLoggedOut
-    | UserLoggedIn { _username :: ST, _sessionCookie :: ST }
+instance ActionM Action
+
+
+class Monad m => ActionLog m where
+    -- | Log events
+    logEvent :: ST -> m ()
+
+instance ActionLog Action where
+    logEvent = liftIO . print
+
+class Monad m => ActionPersist m where
+    -- | Run @Persist@ computation in the action monad.
+    -- Authorization of the action should happen here.
+    persistent :: Persist a -> m a
+
+instance ActionPersist Action where
+    persistent r = ask >>= \(Nat rp) -> liftIO $ rp r
+
+class Monad m => ActionUserHandler m where
+    -- | Make the user logged in
+    login  :: ST -> m ()
+    -- | Make the user log out
+    logout :: m ()
+
+instance ActionUserHandler Action where
+    login username = do
+        put $ UserLoggedIn username "session"
+        persistent $ loginUser username
+
+    logout = do
+        gets _username >>= persistent . logoutUser
+        put UserLoggedOut
+
+class MonadError ActionExcept m => ActionError m
+
+instance ActionError Action
+
+
+----------------------------------------------------------------------
+-- concrete monad type; user state
 
 -- | The actions a user can perform.
 --
@@ -63,33 +104,16 @@ newtype Action a = Action (ExceptT ActionExcept (RWST (Persist :~> IO) () UserSt
              , MonadState UserState
              )
 
-class Monad m => ActionLog m where
-    -- | Log events
-    logEvent :: ST -> m ()
+-- | Top level errors can happen.
+--
+-- FIXME: Create a different type
+type ActionExcept = ServantErr
 
-class Monad m => ActionPersist m where
-    -- | Run @Persist@ computation in the action monad.
-    -- Authorization of the action should happen here.
-    persistent :: Persist a -> m a
-
-class Monad m => ActionUserHandler m where
-    -- | Make the user logged in
-    login  :: ST -> m ()
-    -- | Make the user log out
-    logout :: m ()
-
-class MonadError ActionExcept m => ActionError m
-
-class ( ActionLog m
-      , ActionPersist m
-      , ActionUserHandler m
-      , ActionError m
-      , MonadIO m  -- FIXME: can we get rid of this?  (it is needed in "CreateRandom", but also for
-                   -- 'ActionLog', 'ActionPersist'.)
-      ) => ActionM m
-
-----------------------------------------------------------------------
--- Construction
+-- | User representation during an action
+-- FIXME: Figure out which information is needed here.
+data UserState
+    = UserLoggedOut
+    | UserLoggedIn { _username :: ST, _sessionCookie :: ST }
 
 -- | Creates a natural transformation from Action to IO
 --
@@ -103,27 +127,6 @@ mkRunAction persistNat = \s -> Nat (run s)
     unAction (Action a) = a
     runRWSTflip r s comp = runRWST comp r s
 
-----------------------------------------------------------------------
--- Combinators
-
-instance ActionUserHandler Action where
-    login username = do
-        put $ UserLoggedIn username "session"
-        persistent $ loginUser username
-
-    logout = do
-        gets _username >>= persistent . logoutUser
-        put UserLoggedOut
-
-instance ActionLog Action where
-    logEvent = liftIO . print
-
-instance ActionPersist Action where
-    persistent r = ask >>= \(Nat rp) -> liftIO $ rp r
-
-instance ActionError Action
-
-instance ActionM Action
 
 ----------------------------------------------------------------------
 -- Lens
