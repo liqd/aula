04b1f5e2c006d8761660382ce3eb2151923f9f0f
Matthias Fischmann <mf@zerobuzz.net>
2016-02-27 09:55:55 +0100

    Qualified import: default name.

--- tests/Frontend/PathSpec.hs
+++ tests/Frontend/PathSpec.hs
@@ -5,7 +5,7 @@ where
 
 import Test.Hspec (Spec, describe, it)
 import Test.QuickCheck (Gen, forAll, property)
-import qualified Data.Text as T
+import qualified Data.Text as ST
 
 import Arbitrary
 import Data.UriPath
@@ -15,9 +15,9 @@ spec :: Spec
 spec = do
     describe "HasPath" $ do
         it "absoluteUriPath is not empty and well defined" . property . forAll mainGen $ \path ->
-            T.length (absoluteUriPath $ relPath path) > 0
+            ST.length (absoluteUriPath $ relPath path) > 0
         it "relativeUriPath is not empty and well defined" . property . forAll mainGen $ \path ->
-            T.length (relativeUriPath $ relPath path) > 0
+            ST.length (relativeUriPath $ relPath path) > 0
   where
     mainGen :: Gen Main
     mainGen = arbitrary
