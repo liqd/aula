e1bc40fc03880069cc3eeab26410603d87bdd3f1
Matthias Fischmann <mf@zerobuzz.net>
2016-02-27 09:56:36 +0100

    Absolute paths always contain at least one slash; relative paths may be empty.

--- tests/Frontend/PathSpec.hs
+++ tests/Frontend/PathSpec.hs
@@ -15,9 +15,9 @@ spec :: Spec
 spec = do
     describe "HasPath" $ do
         it "absoluteUriPath is not empty and well defined" . property . forAll mainGen $ \path ->
-            ST.length (absoluteUriPath $ relPath path) > 0
+            ST.length (absoluteUriPath $ relPath path) >= 1
         it "relativeUriPath is not empty and well defined" . property . forAll mainGen $ \path ->
-            ST.length (relativeUriPath $ relPath path) > 0
+            ST.length (relativeUriPath $ relPath path) >= 0
   where
     mainGen :: Gen Main
     mainGen = arbitrary
