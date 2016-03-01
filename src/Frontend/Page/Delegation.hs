c47a5c4f1be8a63ae1587349ef050c34468eb079
Matthias Fischmann <mf@zerobuzz.net>
2016-03-01 11:35:35 +0100

    First draft of delegation network page.

---
+++ src/Frontend/Page/Delegation.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+
+{-# OPTIONS_GHC -Werror -Wall #-}
+
+module Frontend.Page.Delegation
+where
+
+import Prelude
+import Lucid hiding (src_)
+
+import Action
+import Data.UriPath (src_)
+import Frontend.Core
+
+import qualified Frontend.Path as U
+
+
+-- | 13. Delegation network
+data PageDelegationNetwork = PageDelegationNetwork
+  deriving (Eq, Show, Read)
+
+instance Page PageDelegationNetwork where
+    isPrivatePage _ = True
+
+instance ToHtml PageDelegationNetwork where
+    toHtmlRaw = toHtml
+    toHtml p@PageDelegationNetwork = semanticDiv p $ do
+        hr_ []
+        br_ []
+        hr_ []
+
+        let levels = div_ $ do
+                br_ []
+                "  Ebene  "
+                select_ [name_ "level"] $ do
+                    option_ "Schule"
+                    option_ [selected_ "selected"] "Klasse 5f"
+                    option_ "Thema"
+                    option_ "Idee"
+
+                br_ []
+                "  Thema  "
+                select_ [name_ "topic"] $ do
+                    option_ [selected_ "selected"] "Thema 'Kantinenessen'"
+                    option_ [selected_ "selected"] "Thema 'Schulhofmöbel'"
+                    option_ [selected_ "selected"] "Thema 'Saunabereich'"
+
+                br_ []
+                "  Idee  "
+                select_ [name_ "idea"] $ do
+                    option_ [selected_ "selected"] "Idee '1'"
+                    option_ [selected_ "selected"] "Idee '2'"
+                    option_ [selected_ "selected"] "Idee '3'"
+                    option_ [selected_ "selected"] "Idee '4'"
+                    option_ [selected_ "selected"] "Idee '5'"
+                    option_ [selected_ "selected"] "Idee '6'"
+                    option_ [selected_ "selected"] "Idee '7'"
+                    option_ [selected_ "selected"] "Idee '8'"
+                    option_ [selected_ "selected"] "Idee '9'"
+
+        div_ $ do
+            br_ []
+            table_ $ do
+                tr_ $ do
+                    th_ "[angezeigte ebene]"
+                    th_ "[angezeigte schüler]"
+                    th_ "[weggeblendete schüler]"
+                    th_ "[das netzwerk]"
+                tr_ $ do
+                    td_ levels
+                    td_ . ul_ $ li_ `mapM_` ["Hannah", "Hanna", "Leonie", "Leoni", "Lea", "Leah", "Lena"]
+                    td_ . ul_ $ li_ `mapM_` ["Sara", "Emma", "Lilli", "Lilly", "Lili", "Marie", "Lina",
+                                             "Maja", "Maya", "Johanna", "Sophie", "Sofie", "Nele", "Neele",
+                                             "Sophia", "Sofia", "Amelie", "Lisa", "Leni", "Julia", "Alina"]
+                    td_ $ img_ [src_ $ U.TopStatic "pagemap.png", width_ "800"]
+
+        hr_ []
+        br_ []
+        hr_ []
+
+viewDelegationNetwork :: (ActionM m) => m (Frame PageDelegationNetwork)
+viewDelegationNetwork = makeFrame PageDelegationNetwork
