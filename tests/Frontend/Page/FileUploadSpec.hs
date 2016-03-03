5f4e0b9b5034487497e7c4022edcc921a1200b67
Matthias Fischmann <mf@zerobuzz.net>
2016-03-03 15:41:15 +0100

    Add failing test case for csv parser.

--- tests/Frontend/Page/FileUploadSpec.hs
+++ tests/Frontend/Page/FileUploadSpec.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 
 {-# OPTIONS_GHC -Werror -Wall #-}
 
@@ -11,13 +12,18 @@ where
 import Control.Concurrent (forkIO, killThread)
 import Control.Exception
 import Control.Lens
+import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
-import Data.String.Conversions (ST, cs, (<>))
-import Test.Hspec (Spec, describe, it, around, pendingWith)
+import Data.String.Conversions (ST, LBS, cs, (<>))
 import Network.Wreq
+import Test.Hspec (Spec, describe, it, around, pendingWith, shouldBe)
 
-import Frontend
+import qualified Data.ByteString.Lazy.Char8 as LBS
+
+import Action
 import Config
+import Frontend
+import Frontend.Page.FileUpload
 
 
 withServer :: (ST -> IO a) -> IO a
@@ -31,7 +37,8 @@ withServer action = bracket
     -- uri = "http://localhost:8081"
 
 spec :: Spec
-spec = describe "file upload" . around withServer $ do
+spec = describe "file upload" $ do
+  describe "http" . around withServer $ do
     let classPart :: Part
         classPart = partString "/testing/file-upload.classname" "7a"
 
@@ -57,3 +64,37 @@ spec = describe "file upload" . around withServer $ do
 
         r <- postWith opts (cs uri <> "/testing/file-upload") [classPart, filePart]
         liftIO $ print r
+
+  describe "csv file parser" $ do
+        let ts :: [(String, [LBS])]
+            ts = [ ("empty file",
+                    "Vorname;Nachname;email;Login-Name" :
+                    [])
+                 , ("first and last name only",
+                    "Vorname;Nachname;email;Login-Name" :
+                    "Hein;Blöd" :
+                    [])
+                 , ("names and email",
+                    "Vorname;Nachname;email;Login-Name" :
+                    "Hein;Blöd;bloed@example.org" :
+                    [])
+                 , ("names, email, nick",
+                    "Vorname;Nachname;email;Login-Name" :
+                    "Hein;Blöd;bloed@example.org;nick" :
+                    [])
+                 , ("names and nick",
+                    "Vorname;Nachname;email;Login-Name" :
+                    "Hein;Blöd;;nick" :
+                    [])
+                 , ("multiple records",
+                    "Vorname;Nachname;email;Login-Name" :
+                    "Hein;Blöd;bloed@example.org" :
+                    "Heidi;Schmumel;br@example.org;mup" :
+                    "Jens;Kuhn;jens@example.org" :
+                    "Jens2;Kuhn2;;ock" :
+                    [])
+                 ]
+
+        forM_ ts $ \(label, file) -> it label $ do
+            let v :: Either String [CsvUserRecord] = decodeCsv $ LBS.unlines file
+            length <$> v `shouldBe` Right (length file - 1)
