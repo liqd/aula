a4933451c1f7d9978c3e2cae95e41f772a6f23e3
Matthias Fischmann <mf@zerobuzz.net>
2016-03-03 15:42:13 +0100

    Whitespace: indentation only.

--- tests/Frontend/Page/FileUploadSpec.hs
+++ tests/Frontend/Page/FileUploadSpec.hs
@@ -38,34 +38,34 @@ withServer action = bracket
 
 spec :: Spec
 spec = describe "file upload" $ do
-  describe "http" . around withServer $ do
-    let classPart :: Part
-        classPart = partString "/testing/file-upload.classname" "7a"
+    describe "http" . around withServer $ do
+        let classPart :: Part
+            classPart = partString "/testing/file-upload.classname" "7a"
 
-        filePart :: Part
-        filePart = (partFileName .~ Just "x.csv") . (partContentType .~ Just "text/csv") $ p
-          where
-            p = partString "/testing/file-upload.file" $ unlines
-                    [ "Vorname;Nachname;email;Login-Name"
-                    , "Hein;Blöd;bloed@example.org"
-                    , "Heidi;Schmumel;br@example.org;mup"
-                    , "Jens;Kuhn;jens@example.org"
-                    ]
+            filePart :: Part
+            filePart = (partFileName .~ Just "x.csv") . (partContentType .~ Just "text/csv") $ p
+              where
+                p = partString "/testing/file-upload.file" $ unlines
+                        [ "Vorname;Nachname;email;Login-Name"
+                        , "Hein;Blöd;bloed@example.org"
+                        , "Heidi;Schmumel;br@example.org;mup"
+                        , "Jens;Kuhn;jens@example.org"
+                        ]
 
-        opts :: Options
-        opts = defaults
+            opts :: Options
+            opts = defaults
 
-    it "works" $ \uri -> do
-        pendingWith "only partially implemented."
+        it "works" $ \uri -> do
+            pendingWith "only partially implemented."
 
-        -- the response will be a 500 saying "SUCCESS!" and mirroring the parsed csv file back into
-        -- the browser.  what we actually want to test is that the response is a 303 and that the
-        -- database will contain the data we posted.
+            -- the response will be a 500 saying "SUCCESS!" and mirroring the parsed csv file back into
+            -- the browser.  what we actually want to test is that the response is a 303 and that the
+            -- database will contain the data we posted.
 
-        r <- postWith opts (cs uri <> "/testing/file-upload") [classPart, filePart]
-        liftIO $ print r
+            r <- postWith opts (cs uri <> "/testing/file-upload") [classPart, filePart]
+            liftIO $ print r
 
-  describe "csv file parser" $ do
+    describe "csv file parser" $ do
         let ts :: [(String, [LBS])]
             ts = [ ("empty file",
                     "Vorname;Nachname;email;Login-Name" :
