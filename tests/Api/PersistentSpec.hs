f16556580be171609f748e7d2eb249cc7948dae0
Andor Penzes <andor.penzes@gmail.com>
2016-03-07 17:38:36 +0100

    Errata: Regression suite is more descriptive.

--- tests/Api/PersistentSpec.hs
+++ tests/Api/PersistentSpec.hs
@@ -42,7 +42,8 @@ getDbSpec name getXs = do
                 length xs `shouldNotBe` 0
 
 addDbSpecProp :: (Foldable f, Arbitrary proto) =>
-                 String -> Persist (f a) -> (proto -> Persist a) -> (proto -> a -> Bool) -> Spec
+                 String -> Persist (f a) -> (proto -> Persist a) -> (proto -> a -> Expectation) ->
+                 Spec
 addDbSpecProp name getXs addX propX =
     describe name $ do
         let t = it "adds one" $ \(Nat rp) -> do
@@ -51,14 +52,14 @@ addDbSpecProp name getXs addX propX =
                     r <- liftIO . rp $ addX p
                     after' <- liftIO $ length <$> rp getXs
                     after' `shouldBe` before' + 1
-                    propX p r `shouldBe` True
+                    propX p r
 
         context "on empty database" . before mkEmpty $ t
         context "on initial database" . before mkInitial $ t
 
 addDbSpec :: (Foldable f, Arbitrary proto) =>
              String -> Persist (f a) -> (proto -> Persist a) -> Spec
-addDbSpec name getXs addX = addDbSpecProp name getXs addX (\_ _ -> True)
+addDbSpec name getXs addX = addDbSpecProp name getXs addX (\_ _ -> passes)
 
 findInBySpec :: (Eq a, Show a, Arbitrary k) =>
                 String -> Persist [a] -> (k -> Persist (Maybe a)) ->
@@ -182,7 +183,14 @@ spec = do
 -- * Regression suite
 
 regression :: Spec
-regression = context "regression" $ do
-    addDbSpecProp
-        "addIdea"  getIdeas addIdea
-        (\p i -> i ^. ideaSpace == p ^. protoIdeaIdeaSpace)
+regression = describe "regression" $ do
+    describe "IdeaSpace in proto idea and saved idea should be the same" $
+        addDbSpecProp
+            "addIdea" getIdeas addIdea
+            (\p i -> i ^. ideaSpace `shouldBe` p ^. protoIdeaIdeaSpace)
+
+----------------------------------------------------------------------
+-- Expectations
+
+passes :: Expectation
+passes = return ()
