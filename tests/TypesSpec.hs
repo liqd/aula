c959d41d2f4034fdcab60b8eb228c12a29713f6d
Matthias Fischmann <mf@zerobuzz.net>
2016-03-11 16:24:20 +0100

    Cleanup.

--- tests/TypesSpec.hs
+++ tests/TypesSpec.hs
@@ -1,26 +1,23 @@
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module TypesSpec where
-
-import Arbitrary ()
-import Data.Monoid ((<>))
-import Types
 
 import Data.Binary (encode, decode)
 import Data.Maybe (isJust, isNothing)
-
+import Data.Monoid ((<>))
 import Test.Hspec (Spec, describe, it)
 import Test.QuickCheck (property)
 
+import Arbitrary ()
+import Types
+
+
 spec :: Spec
 spec = do
-    timestamp
-
-timestamp = describe "Timestamp" $ do
-    it "encode and decode are inverses" . property $
-        \x -> decode (encode x) == (x :: Timestamp)
-    it "read and show are inverses" . property $
-        \x -> read (show x) == (x :: Timestamp)
-    it "parseTimestamp and renderTimestamp are inverses" . property $
-        isJust . parseTimestamp . renderTimestamp
-    it "parseTimestamp should fail on noise" . property $
-        isNothing . parseTimestamp . (<> "noise") . renderTimestamp
+    describe "Timestamp" $ do
+        it "encode and decode are inverses" . property $
+            \x -> decode (encode x) == (x :: Timestamp)
+        it "read and show are inverses" . property $
+            \x -> read (show x) == (x :: Timestamp)
+        it "parseTimestamp and renderTimestamp are inverses" . property $
+            isJust . parseTimestamp . renderTimestamp
+        it "parseTimestamp should fail on noise" . property $
+            isNothing . parseTimestamp . (<> "noise") . renderTimestamp
