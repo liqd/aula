41486845f82a5bb96cad20e2902b6a499d6ca22f
Matthias Fischmann <mf@zerobuzz.net>
2016-03-14 17:19:16 +0100

    Remove dangling whitespace.

--- src/Arbitrary.hs
+++ src/Arbitrary.hs
@@ -342,10 +342,10 @@ instance Arbitrary P.AdminPs where
 -- servant-mock
 
 instance Arbitrary a => Arbitrary (FormPage a) where
-    arbitrary = FormPage <$> arb <*> pure (return ()) 
+    arbitrary = FormPage <$> arb <*> pure (return ())
 
 instance Arbitrary a => Arbitrary (Frame a) where
-    arbitrary = oneof [ Frame <$> arb <*> arb, PublicFrame <$> arb ] 
+    arbitrary = oneof [ Frame <$> arb <*> arb, PublicFrame <$> arb ]
 
 instance Arbitrary a => Arbitrary (Frame' a)where
     arbitrary = oneof [ Frame' <$> arb <*> pure (pure ()) <*> arb, PublicFrame' (pure ()) <$> arb ]
