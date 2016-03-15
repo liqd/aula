b618264d51199aba54dfd7990a553576a1cba23a
Matthias Fischmann <mf@zerobuzz.net>
2016-03-15 22:45:22 +0100

    Whitespace.

--- src/Frontend/Core.hs
+++ src/Frontend/Core.hs
@@ -176,19 +176,22 @@ headerMarkup mUser = header_ [class_ "main-header"] $ do
                 div_ [class_ "pop-menu"] $ do
                         "Hi " <> (usr ^. userLogin . fromUserLogin . html)
                         ul_ [class_ "pop-menu-list"] $ do
-                            li_ [class_ "pop-menu-list-item"] . a_ [href_ $ P.User (usr ^. _Id) P.UserIdeas] $ do
+                            li_ [class_ "pop-menu-list-item"]
+                                . a_ [href_ $ P.User (usr ^. _Id) P.UserIdeas] $ do
                                 i_ [class_ "pop-menu-list-icon icon-eye"] nil
                                 "Profil anzeigen"
-                            li_ [class_ "pop-menu-list-item"] . a_ [href_ P.UserSettings] $ do
+                            li_ [class_ "pop-menu-list-item"]
+                                . a_ [href_ P.UserSettings] $ do
                                 i_ [class_ "pop-menu-list-icon icon-sun-o"] nil
                                 "Einstellungen"
-                            li_ [class_ "pop-menu-list-item"] . a_ [href_ $ P.Admin P.AdminDuration] $ do
+                            li_ [class_ "pop-menu-list-item"]
+                                . a_ [href_ $ P.Admin P.AdminDuration] $ do
                                 i_ [class_ "pop-menu-list-icon icon-bolt"] nil
                                 "Prozessverwaltung"
-                            li_ [class_ "pop-menu-list-item"] . a_ [href_ P.Logout] $ do
+                            li_ [class_ "pop-menu-list-item"]
+                                . a_ [href_ P.Logout] $ do
                                 i_ [class_ "pop-menu-list-icon icon-power-off"] nil
                                 "Logout"
-
             Nothing -> nil
         div_ [class_ "user-avatar"] $ do
             nil
