4485ef3e46d1c679085421661cf85329158416c3
Matthias Fischmann <mf@zerobuzz.net>
2016-03-28 16:21:34 +0200

    Story testing: ActionM interpreter.

--- tests/AulaTests/Stories.hs
+++ tests/AulaTests/Stories.hs
@@ -4,12 +4,15 @@
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE ViewPatterns          #-}
 
 {-# OPTIONS_GHC #-}
 
 module AulaTests.Stories where
 
+import Prelude hiding ((.), id)
+import Control.Category
 import Control.Monad.Free
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
@@ -18,7 +21,7 @@ import Data.String
 import Data.String.Conversions
 import Data.Typeable (Typeable, typeOf)
 import Lucid (Html, ToHtml, toHtml, renderText)
-import Servant (unNat)
+import Servant
 import Servant.Server.Internal.ServantErr
 import Test.Hspec
 import Test.QuickCheck
@@ -27,7 +30,12 @@ import Text.Digestive.View
 
 import qualified Data.Text.Lazy as LT
 
+import qualified Action
+import CreateRandom
+import Action.Implementation
 import Arbitrary
+import Config
+import Persistent.Implementation.STM
 import Types
 
 import AulaTests.Stories.DSL
@@ -38,9 +46,21 @@ spec :: Spec
 spec = describe "stories" $ it "works" $ do
     liftIO $ do
         print "---------------------------"
-        run program
+        runAction program
         print "---------------------------"
     True `shouldBe` True
+
+
+runAction :: Behavior a -> IO a
+runAction program = do
+    persist <- Persistent.Implementation.STM.mkRunPersist
+
+    let runAction :: Action Persistent.Implementation.STM.Persist :~> IO
+        runAction = Nat (fmap (either (error . show) id) . runExceptT)
+                  . mkRunAction (Action.ActionEnv persist Config.devel)
+
+    unNat persist genInitialTestDb
+    unNat runAction $ AulaTests.Stories.Interpreter.Action.run program
 
 
 program :: Behavior ()
--- tests/AulaTests/Stories/Interpreter/Action.hs
+++ tests/AulaTests/Stories/Interpreter/Action.hs
@@ -20,22 +20,54 @@ import Data.Typeable (Typeable, typeOf)
 import Lucid (Html, ToHtml, toHtml, renderText)
 import Servant (unNat)
 import Servant.Server.Internal.ServantErr
-import Test.Hspec  -- (Spec, context, it, pendingWith, shouldBe)
-import Test.QuickCheck  -- (Arbitrary(..), Gen, forAll, property)
+import Test.Hspec
+import Test.QuickCheck
 import Text.Digestive.Types
 import Text.Digestive.View
 
 import qualified Data.Text.Lazy as LT
 
+import Action
+import Persistent
 import Arbitrary
 import Types
 
 import AulaTests.Stories.DSL
 
 
-run :: Behavior a -> IO a
-run (Pure r)                     = pure r
-run (Free (Login l k))           = print ("logged in: " <> show l) >> run k
-run (Free (Logout k))            = print "logged out" >> run k
-run (Free (SelectIdeaSpace s k)) = print ("select idea space: " <> show s) >> run k
-run (Free (CreateIdea pi k))     = print ("create idea: " <> show pi) >> run k
+runLog :: Behavior a -> IO a
+runLog (Pure r)                     = pure r
+runLog (Free (Login l k))           = print ("logged in: " <> show l)         >> runLog k
+runLog (Free (Logout k))            = print "logged out"                      >> runLog k
+runLog (Free (SelectIdeaSpace s k)) = print ("select idea space: " <> show s) >> runLog k
+runLog (Free (CreateIdea pi k))     = print ("create idea: " <> show pi)      >> runLog k
+
+run :: (ActionM r m) => Behavior a -> m a
+run (Pure r) = pure r
+
+run (Free (Login l k)) = do
+    Just u <- persistent $ findUserByLogin l
+    Action.login u
+    u' <- currentUser
+    assert (u, u') (u == u')
+    run k
+
+run (Free (Logout k)) = do
+    Action.logout >> run k
+
+run (Free (SelectIdeaSpace s k)) = do
+    let (Right i :: Either String IdeaSpace) = parseIdeaSpace s
+    persistent $ addIdeaSpaceIfNotExists i
+    run k
+
+run (Free (CreateIdea pi k)) = do
+    Action.currentUserAddDb Persistent.addIdea pi
+    run k
+
+
+-- * helpers
+
+assert :: (Show msg, ActionM r m) => msg -> Bool -> m ()
+assert _ True  = return ()
+assert msg False = error $ "assertion failed: " <> show msg
+    -- FIXME: give source code location of the call.
