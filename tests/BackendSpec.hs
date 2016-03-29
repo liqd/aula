1729e1eee5908b80eb6f6c14c936f7b6861f7637
Matthias Fischmann <mf@zerobuzz.net>
2016-03-29 11:57:09 +0200

    Errata.

--- tests/BackendSpec.hs
+++ tests/BackendSpec.hs
@@ -7,5 +7,5 @@ spec = do
     describe "delegation graph" . around withServer $ do
         it "responds" $ \query -> do
             get query "/api/delegations" `shouldRespond` [codeShouldBe 200]
-        it "body contains delegation network" $ \query -> do
+        it "body contains delegation network" $ \_query -> do
             pending
