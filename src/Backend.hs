2c2d2520153363fca44510e6fa429d1e74aa0bf6
Matthias Fischmann <mf@zerobuzz.net>
2016-03-31 18:21:12 +0200

    Fix: typo.

--- src/Backend.hs
+++ src/Backend.hs
@@ -28,5 +28,5 @@ type DelegationsApi = Get '[JSON] DelegationNetwork
 -- | FIXME: This is all a bit silly: the new end-point logs in admin implicitly; the returned
 -- delegation networks are generated on top of the existing data; testing doesn't really test
 -- anything.  But it is self-contained and a good basis to continue from.
-delegationsApi :: (GenArbitrary r, ActionM r m) => ServerT Api m
+delegationsApi :: (GenArbitrary r, ActionM r m) => ServerT DelegationsApi m
 delegationsApi = Action.loginByName "admin" >> fishDelegationNetworkAction
