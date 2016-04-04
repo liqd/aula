a59fa87f14d7d475fe34546d6f9ac76a912ab06a
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-04-04 18:09:31 +0200

    Remove nused imports of Persistent.Api

--- src/Persistent/Api.hs
+++ src/Persistent/Api.hs
@@ -24,32 +24,11 @@ module Persistent.Api
 -}
 where
 
-import Control.Exception (finally)
 import Control.Lens
-import Control.Monad.Error.Class (MonadError)
-import Control.Monad.Except (ExceptT)
 import Control.Monad.Reader (ask)
-import Control.Monad (unless, replicateM, when)
 import Data.Acid hiding (makeAcidic)
-import Data.Acid.Core
-import Data.Elocrypt (mkPassword)
-import Data.Foldable (find, for_)
-import Data.List (nub)
-import Data.Maybe (fromMaybe)
 import Data.SafeCopy (base, deriveSafeCopy)
-import Data.Set (Set)
-import Data.String.Conversions (ST, cs, (<>))
-import Data.Time.Clock (getCurrentTime)
-import Data.Typeable (Typeable)
-import Servant (ServantErr)
-import Servant.Missing (ThrowError500(..))
-import Servant.Server ((:~>))
 
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import qualified Data.Text as ST
-
-import Types
 import Persistent.Pure
 import Persistent.Idiom
 import Persistent.TemplateHaskell (makeAcidic)
