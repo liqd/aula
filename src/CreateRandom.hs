3f14bebae2ef32cd21dd9cc3b039e22b53b80c2a
Matthias Fischmann <mf@zerobuzz.net>
2016-04-06 11:12:42 +0200

    Fix type errors.

--- src/CreateRandom.hs
+++ src/CreateRandom.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
 
 {-# OPTIONS_GHC -Werror -Wall #-}
 
@@ -20,32 +20,76 @@ import Arbitrary ()
 -- plus one extra user for logging test.
 --
 -- Note that no user is getting logged in by this code.
-genInitialTestDb :: (ActionM m) => m ()
+genInitialTestDb :: (MonadIO m, ActionM m) => m ()
 genInitialTestDb = do
-    pure ()  -- TODO recover this, but make it deterministic.
-    {-
-    addIdeaSpaceIfNotExists SchoolSpace
-    addIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "7a")
-    addIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "7b")
-    addIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "8a")
-    protoU <- genArbitrary
-    firstUser <- addFirstUser $ protoU & protoUserLogin    ?~ UserLogin "admin"
-                                       & protoUserPassword ?~ UserPassInitial "pssst"
-    protoU2 <- genArbitrary
-    user2 <- addUser (firstUser,
-                      protoU2 & protoUserLogin ?~ UserLogin "admin2"
-                              & protoUserPassword ?~ UserPassInitial "pssst2")
-    _wildIdea <- addIdea . (,) firstUser . (protoIdeaLocation . ideaLocationMaybeTopicId .~ Nothing) =<< genArbitrary
-    topicIdea <- addIdea . (,) user2     =<< genArbitrary
-    _topic <- addTopic . (,) firstUser . (protoTopicIdeas .~ [topicIdea ^. _Id]) =<< genArbitrary
+    aupdate $ AddIdeaSpaceIfNotExists SchoolSpace
+    aupdate $ AddIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "7a")
+    aupdate $ AddIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "7b")
+    aupdate $ AddIdeaSpaceIfNotExists $ ClassSpace (SchoolClass 2016 "8a")
+
+    user1 <- aupdate $ AddFirstUser sometime ProtoUser
+        { _protoUserLogin     = Just "admin"
+        , _protoUserFirstName = "A."
+        , _protoUserLastName  = "Admin"
+        , _protoUserRole      = Admin
+        , _protoUserPassword  = Just (UserPassInitial "pssst")
+        , _protoUserEmail     = Nothing
+        }
+
+    loginByUser user1
+
+    user2 <- currentUserAddDb (AddUser (UserPassInitial "geheim")) ProtoUser
+        { _protoUserLogin     = Just "godmin"
+        , _protoUserFirstName = "G."
+        , _protoUserLastName  = "Godmin"
+        , _protoUserRole      = Admin
+        , _protoUserPassword  = Just (UserPassInitial "geheim")
+        , _protoUserEmail     = Nothing
+        }
+
+    _wildIdea <- currentUserAddDb AddIdea ProtoIdea
+            { _protoIdeaTitle    = "wild-idea-title"
+            , _protoIdeaDesc     = Markdown "wild-idea-desc"
+            , _protoIdeaCategory = CatRule
+            , _protoIdeaLocation = IdeaLocationSpace SchoolSpace
+            }
+
+    logout
+    loginByUser user2
+
+    topicIdea <- currentUserAddDb AddIdea ProtoIdea
+            { _protoIdeaTitle    = "topic-idea-title"
+            , _protoIdeaDesc     = Markdown "topic-idea-desc"
+            , _protoIdeaCategory = CatRule
+            , _protoIdeaLocation = IdeaLocationSpace SchoolSpace
+            }
+
+    logout
+    loginByUser user1
+
+    topic <- currentUserAddDb AddTopic ProtoTopic
+        { _protoTopicTitle     = "topic-title"
+        , _protoTopicDesc      = Markdown "topic-desc"
+        , _protoTopicImage     = ""
+        , _protoTopicIdeaSpace = SchoolSpace
+        , _protoTopicIdeas     = []
+        , _protoTopicRefinDays = sometime
+        }
+
+    aupdate $ MoveIdeasToLocation [topicIdea ^. _Id] (topicIdeaLocation topic)
+
+    logout
     return ()
-    -}
+
+
+sometime :: Timestamp
+sometime = Timestamp $ read "2016-02-20 17:09:23.325662 UTC"
+
 
 -- FIXME
 frameUserHack :: User
 frameUserHack = user
   where
-    sometime = Timestamp $ read "2016-02-20 17:09:23.325662 UTC"
     user = User
       { _userMeta      = metainfo
       , _userLogin     = "VorNam"
