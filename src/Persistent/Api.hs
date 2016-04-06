6276c787f1f18d9e2838665fac2623a353332996
Matthias Fischmann <mf@zerobuzz.net>
2016-04-06 10:03:55 +0200

    Cleanup.

--- src/Persistent/Api.hs
+++ src/Persistent/Api.hs
@@ -15,20 +15,11 @@
 {-# OPTIONS_GHC -Wall -Werror -fno-warn-orphans #-}
 
 module Persistent.Api
-{-
-    ( RunPersistT(..), rpDesc, rpState, rpClose
-    , RunPersist
-    , module Persistent.Pure
-    , module Persistent.Idiom
-    )
--}
 where
 
-import Control.Exception (finally)
 import Control.Lens
 import Control.Monad.Reader (ask)
 import Data.Acid hiding (makeAcidic)
-import Data.Monoid
 import Data.SafeCopy (base, deriveSafeCopy)
 
 import Persistent.Pure
@@ -48,16 +39,6 @@ data RunPersistT m =
 makeLenses ''RunPersistT
 
 type RunPersist = RunPersistT IO
-
--- | A more low-level variant of 'Persistent.Implementation.withPersist' with the implementation
--- explicit as parameter.
---
--- TODO: move to "Persistent.Implementation"?  or move both @withPersist*@s here?
-withPersist' :: IO RunPersist -> (RunPersist -> IO a) -> IO a
-withPersist' mkRunP m = do
-    rp@(RunPersist desc _ close) <- mkRunP  -- initialization happens here
-    putStrLn $ "persistence: " <> desc -- FIXME: use logger for this (or perhaps log in the construction of Action, where we have a logger?)
-    m rp `finally` close  -- closing happens here
 
 askDb :: Query AulaData AulaData
 askDb = ask
--- src/Persistent/Implementation.hs
+++ src/Persistent/Implementation.hs
@@ -2,17 +2,28 @@
 
 {-# OPTIONS_GHC -Wall -Werror #-}
 
-module Persistent.Implementation (mkRunPersist, withPersist)
+module Persistent.Implementation (mkRunPersist, withPersist, withPersist')
 where
 
-import Control.Lens
 import Config
+import Control.Exception (finally)
+import Control.Lens
+import Data.Monoid
 import Persistent.Api
 import Persistent.Implementation.AcidState
 
 withPersist :: Config -> (RunPersist -> IO a) -> IO a
 withPersist = withPersist' . mkRunPersist
 
+-- | A more low-level variant of 'Persistent.Implementation.withPersist' with the implementation
+-- explicit as parameter.
+withPersist' :: IO RunPersist -> (RunPersist -> IO a) -> IO a
+withPersist' mkRunP m = do
+    rp@(RunPersist desc _ close) <- mkRunP  -- initialization happens here
+    putStrLn $ "persistence: " <> desc -- FIXME: use logger for this (or perhaps log in the construction of Action, where we have a logger?)
+    m rp `finally` close  -- closing happens here
+
+-- TODO: give this a different name.
 mkRunPersist :: Config -> IO RunPersist
 mkRunPersist cfg =
     case cfg ^. persistenceImpl of
