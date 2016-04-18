40791528898116369a5595549f462409d7c9a215
Matthias Fischmann <mf@zerobuzz.net>
2016-04-18 14:24:07 +0200

    User settings form redirects to itself.

--- src/Frontend/Page/User.hs
+++ src/Frontend/Page/User.hs
@@ -57,7 +57,11 @@ instance FormPage PageUserSettings where
     type FormPagePayload PageUserSettings = UserSettingData
 
     formAction _ = U.UserSettings
-    redirectOf _ _ = U.ListSpaces  -- FIXME: Redirect to the right place
+
+    -- Redirect to ourselves, so the user can review the changes.  FUTUREWORK: It would be nice to
+    -- have a messaging device that prints a line "your changes have been saved" at the top of the
+    -- form; without that, UX is still a bit confusing.
+    redirectOf _ _ = U.UserSettings
 
     makeForm (PageUserSettings user) =
         UserSettingData
