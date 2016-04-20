8908491f6d7cc17a7e4d5b327ebb31505c1535ac
Matthias Fischmann <mf@zerobuzz.net>
2016-04-20 11:57:14 +0200

    Errata.

--- src/DemoData.hs
+++ src/DemoData.hs
@@ -10,7 +10,7 @@ where
 
 import Control.Applicative ((<**>))
 import Control.Exception (assert)
-import Control.Lens (Getter, (^.), (^?), (.~), (#), (&), set, re, pre, _Just)
+import Control.Lens (Getter, (^.), (^?), (.~), (&), set, re, pre, _Just)
 import Control.Monad (zipWithM_)
 import Data.List (nub)
 import Data.Maybe (mapMaybe)
