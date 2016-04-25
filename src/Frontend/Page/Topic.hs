62da91c94898e4616720e064410cae3b44dd2f13
Caroline Clifford <me@carolineclifford.com>
2016-04-25 17:55:01 +0200

    Move next phase button to menu from header

--- src/Frontend/Page/Topic.hs
+++ src/Frontend/Page/Topic.hs
@@ -121,6 +121,18 @@ viewTopicHeaderDiv ctx topic tab = do
                         a_ [id_ "edit-topic",  href_ . U.Space space $ U.MoveIdeasToTopic topicId] $ do
                             i_ [class_ "icon-pencil"] nil
                             "Thema bearbeiten"
+                    when (CanPhaseForwardTopic `elem` caps) $
+                        li_ [class_ "pop-menu-list-item m-form"] $ do
+                            div_ [class_ "pop-menu-list-item-form-wrapper"] $ do
+                                i_ [class_ "icon-step-forward"] nil
+                                postLink_
+                                    [class_ "btn-plain", onclickJs jsReloadOnClick]
+                                    (U.Admin $ U.AdminTopicNextPhase topicId)
+                                    "Nächste Phase"
+
+
+
+
         h1_   [class_ "main-heading"] $ do
             span_ [class_ "sub-heading"] . toHtml $ phaseName phase
             toHtml $ topic ^. topicTitle
