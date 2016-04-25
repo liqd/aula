fc43bf9792ad9102c49c300a3554dcf118370716
Matthias Fischmann <mf@zerobuzz.net>
2016-04-25 09:31:41 +0200

    Add FIXME.

--- src/Logger.hs
+++ src/Logger.hs
@@ -18,6 +18,11 @@ data LogLevel
     | NOLOG
   deriving (Eq, Generic, Ord, Show, FromJSON, ToJSON)
 
+-- FIXME: EventLog should be merged with this module and everything in it renamed to something
+-- saying `moderator`; ST should change into an ADT that has a constructor for EventLogItems, but
+-- also other constructors for things like `disk full`.  The logger can then have several targets
+-- (moderator event log, syslog, admin email, devops email, ...), and decide what to do with each
+-- event based on its type.
 data LogEntry = LogEntry LogLevel ST
   deriving (Eq, Show)
 
