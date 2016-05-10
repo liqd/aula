66997f3d285f223c4c631f65d49281adc236aea7
Andor Penzes <andor.penzes@gmail.com>
2016-05-10 11:46:40 +0200

    Render "on the table" icon for only wild ideas.

--- src/Types.hs
+++ src/Types.hs
@@ -530,6 +530,11 @@ followsPhase (PhaseVoting _) PhaseJury           = True
 followsPhase PhaseResult     (PhaseVoting _)     = True
 followsPhase _               _                   = False
 
+wildPhase :: Phase -> Bool
+wildPhase = \case
+    PhaseWildIdea     -> True
+    PhaseWildFrozen   -> True
+    _                 -> False
 
 -- * user
 
