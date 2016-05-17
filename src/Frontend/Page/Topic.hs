d49f24a1814616c3b1dc8f4e8a63fe9ff8f33215
Andor Penzes <andor.penzes@gmail.com>
2016-05-17 22:56:10 +0200

    Errata.

--- src/Frontend/Page/Topic.hs
+++ src/Frontend/Page/Topic.hs
@@ -177,16 +177,12 @@ viewTopicHeaderDiv now ctx topic tab = do
             topic ^. topicTitle . html
         p_ [class_ "sub-header"] $ topic ^. topicDesc . html
         div_ [class_ "heroic-btn-group"] $ do
-            let createIdeaButton
-                  | CanCreateIdea `notElem` caps = nil
-                  | otherwise = do
+            let createIdeaButton = when (CanCreateIdea `elem` caps) $
                     a_ [ class_ "btn-cta heroic-cta"
                        , href_ . U.createIdea $ IdeaLocationTopic space topicId
                        ]
-                     "+ Neue Idee"
-                delegateVoteButton
-                  | CanVoteTopic `notElem` caps = nil
-                  | otherwise = do
+                        "+ Neue Idee"
+                delegateVoteButton = when (CanVoteTopic `elem` caps) $
                     a_  [ class_ "btn-cta heroic-cta"
                         , href_ . U.Space space $ U.CreateTopicDelegation topicId
                         ] $ do
