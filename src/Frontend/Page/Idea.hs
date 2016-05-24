e7a286299299f67a85f9674dcde97903fb804ee7
Caroline Clifford <me@carolineclifford.com>
2016-05-24 16:40:12 +0200

    fix up feasable in detail header fixes #509

--- src/Frontend/Page/Idea.hs
+++ src/Frontend/Page/Idea.hs
@@ -339,8 +339,8 @@ feasibilityVerdict renderJuryButtons idea caps = div_ [id_ . U.anchor $ idea ^. 
     case _ideaJuryResult idea of
         Nothing -> nil
         Just (IdeaJuryResult _ (Feasible maybeExpl)) -> do
-            div_ [class_ "info-text m-realised"] $ do
-                h3_ [class_ "info-text-header"] "durchführbar"
+            div_ [class_ "icon-list m-display-only m-inline"] $ ul_ $ do
+                li_ [class_ "icon-feasable"] $ span_ "durchführbar"
                 maybeExpl ^. _Just . to explToHtml
         Just (IdeaJuryResult _ (NotFeasible expl)) -> do
             div_ [class_ "info-text m-unrealised"] $ do
