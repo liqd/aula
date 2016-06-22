5c0fb9658c6ed3175acea71700e24b2a3dfc6d98
Matthias Fischmann <mf@zerobuzz.net>
2016-06-22 17:00:02 +0200

    ...

--- static/d3-aula.js
+++ static/d3-aula.js
@@ -311,9 +311,7 @@
             .linkDistance(70)
             .start();
 
-        var controls = d3.select(rootSel).append("textArea").on("change", function() {
-            filterByPower(this.value);
-        });
+        initializeControlPanel(rootSel, filterByPower);
 
         var svg = d3.select(rootSel).append("svg")
             .attr("width", width)
@@ -337,78 +335,17 @@
         var text = undefined;
 
         updateWidget();
+    };
 
-/*
+    var initializeControlPanel = function(rootSel, filterByPower) {
+        var controls = d3.select(rootSel);
 
-var margin = {top: 200, right: 50, bottom: 200, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.bottom - margin.top;
-
-var x = d3.scale.linear()
-    .domain([0, 180])
-    .range([0, width])
-    .clamp(true);
-
-var brush = d3.svg.brush()
-    .x(x)
-    .extent([0, 0])
-    .on("brush", brushed);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height / 2 + ")")
-    .call(d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .tickFormat(function(d) { return d + "°"; })
-      .tickSize(0)
-      .tickPadding(12))
-  .select(".domain")
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "halo");
-
-var slider = svg.append("g")
-    .attr("class", "slider")
-    .call(brush);
-
-slider.selectAll(".extent,.resize")
-    .remove();
-
-slider.select(".background")
-    .attr("height", height);
-
-var handle = slider.append("circle")
-    .attr("class", "handle")
-    .attr("transform", "translate(0," + height / 2 + ")")
-    .attr("r", 9);
-
-slider
-    .call(brush.event)
-  .transition() // gratuitous intro!
-    .duration(750)
-    .call(brush.extent([70, 70]))
-    .call(brush.event);
-
-function brushed() {
-  var value = brush.extent()[0];
-
-  if (d3.event.sourceEvent) { // not a programmatic event
-    value = x.invert(d3.mouse(this)[0]);
-    brush.extent([value, value]);
-  }
-
-  handle.attr("cx", x(value));
-  d3.select("body").style("background-color", d3.hsl(value, .8, .8));
-}
-
-*/
-
+        controls.append("label").text("Nur Delegierte mit mindestens ");
+        controls.append("input")
+            .attr("type", "number")
+            .on("keyup",   function() { filterByPower(this.value); })
+            .on("mouseup", function() { filterByPower(this.value); });
+        controls.append("label").text(" Stimmen anzeigen.");
     };
 
 
