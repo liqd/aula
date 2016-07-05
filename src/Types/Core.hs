be30339e355a0d292419e33182c26e2a64a6339b
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-07-05 15:03:36 +0200

    hlint: remove unneeded DefaultSignatures

--- src/Types/Core.hs
+++ src/Types/Core.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE ConstraintKinds             #-}
 {-# LANGUAGE DataKinds                   #-}
-{-# LANGUAGE DefaultSignatures           #-}
 {-# LANGUAGE DeriveGeneric               #-}
 {-# LANGUAGE FlexibleContexts            #-}
 {-# LANGUAGE FlexibleInstances           #-}
