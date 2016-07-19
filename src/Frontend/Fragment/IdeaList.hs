791952497d1f81f60d155ab9566549f0f3009725
Matthias Fischmann <mf@zerobuzz.net>
2016-07-19 17:02:56 +0200

    Drop "not winning" (aka "hourglass") from idea list view; show winning badge conditionally.

--- src/Frontend/Fragment/IdeaList.hs
+++ src/Frontend/Fragment/IdeaList.hs
@@ -84,11 +84,9 @@ instance ToHtml ListItemIdea where
                             div_ [class_ "m-table indicator-item"] $ do
                                   div_ [class_ "indicator-icon"] "Kann auf den Tisch"
 
-                    -- FIXME - add some logic pls :) #859
-                    div_ [class_ "icon-winner indicator-item m-inline m-display-only"] $ do
-                        div_ [class_ "indicator-icon"] "gewonnen"
-                    div_ [class_ "icon-hourglass indicator-item m-inline m-display-only"] $ do
-                        div_ [class_ "indicator-icon"] "nicht gewonnen"
+                    when (isWinning idea) $ do
+                        div_ [class_ "icon-winner indicator-item m-inline m-display-only"] $ do
+                            div_ [class_ "indicator-icon"] "gewonnen"
 
                     case idea ^? ideaJuryResult . _Just . ideaJuryResultValue . to ideaJuryResultValueToType of
                         Nothing              -> nil  -- (not judged)
