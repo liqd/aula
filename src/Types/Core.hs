47941f450b7b8a611efe8fe66b9c4d17682327ab
Matthias Fischmann <mf@zerobuzz.net>
2016-07-20 14:38:09 +0200

    New category "activities".

--- src/Types/Core.hs
+++ src/Types/Core.hs
@@ -284,6 +284,7 @@ type instance Proto Idea = ProtoIdea
 data Category =
     CatRules        -- ^ "Regel"
   | CatEquipment    -- ^ "Ausstattung"
+  | CatActivities   -- ^ "Aktivitäten"
   | CatTeaching     -- ^ "Unterricht"
   | CatTime         -- ^ "Zeit"
   | CatEnvironment  -- ^ "Umgebung"
@@ -678,6 +679,7 @@ instance HasUILabel Category where
     uilabel = \case
         CatRules       -> "Regeln"
         CatEquipment   -> "Ausstattung"
+        CatActivities  -> "Aktivitäten"
         CatTeaching    -> "Unterricht"
         CatTime        -> "Zeit"
         CatEnvironment -> "Umgebung"
@@ -686,6 +688,7 @@ instance ToHttpApiData Category where
     toUrlPiece = \case
         CatRules       -> "rules"
         CatEquipment   -> "equipment"
+        CatActivities  -> "activities"
         CatTeaching    -> "teaching"
         CatTime        -> "time"
         CatEnvironment -> "environment"
@@ -694,6 +697,7 @@ instance FromHttpApiData Category where
     parseUrlPiece = \case
         "rules"       -> Right CatRules
         "equipment"   -> Right CatEquipment
+        "activities"  -> Right CatActivities
         "teaching"    -> Right CatTeaching
         "time"        -> Right CatTime
         "environment" -> Right CatEnvironment
