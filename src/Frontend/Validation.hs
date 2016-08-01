4e3e26ae5087ff4bbb366063809a38b767b66738
Matthias Fischmann <mf@zerobuzz.net>
2016-08-01 11:13:08 +0200

    German.

--- src/Frontend/Validation.hs
+++ src/Frontend/Validation.hs
@@ -248,7 +248,7 @@ optionalEmailField name emailValue =
 
 checkEmail :: (Monad m) => FieldName -> ST -> DF.Result (HtmlT m ()) Frontend.EmailAddress
 checkEmail name value = case Email.emailAddress (cs value) of
-    Nothing -> DF.Error . fromString $ unwords [name, ":", "Ungültige email-Adresse"]
+    Nothing -> DF.Error . fromString $ unwords [name, ":", "Emailadresse ungültig"]
     Just e  -> DF.Success $ InternalEmailAddress e
 
 -- Same as 'optionalEmailField' but the email value is required.
