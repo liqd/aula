b6fcb72005b0d2048f060c266f755bb045735cef
Andor Penzes <andor.penzes@gmail.com>
2016-08-08 13:07:28 +0200

    Expose default ekg port in docker container.

--- docker/run.sh
+++ docker/run.sh
@@ -15,5 +15,5 @@ fi
 if [ "$CONNECT_TO_RUNNING_CONTAINER" = "1" ]; then
     docker exec -it `docker ps -q --filter="ancestor=$AULA_IMAGE"` /bin/bash
 else
-    docker run -it --rm -p 8080:8080 -p 5900:5900 $VOLUMES $AULA_IMAGE /bin/bash
+    docker run -it --rm -p 8080:8080 -p 5900:5900 -p 8888:8888 $VOLUMES $AULA_IMAGE /bin/bash
 fi
